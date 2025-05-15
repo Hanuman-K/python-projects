13201aab1eb97d6fff8059334f948f0577979ce5
Hanuman-K <kichannagarih@gmail.com>
2025-05-15 08:07:03 +0530

    file2

---
+++ p4.py
@@ -0,0 +1,15 @@
+name= 'sonu'
+naam="'man'"
+print(name+naam)
+print(name)
+print(name+'rocks')
+print(name+' rickls')
+print(name[1],1)
+print(name[0:2],2)
+print(name[1:],3)
+print(name[:1],4)
+print('my '+name[3: ])
+myname='kichannagari hanuman'
+print(len(myname))
+
+
